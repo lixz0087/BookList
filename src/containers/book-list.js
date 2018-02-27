b2e455ba7f59888b8c2ab2514a295f0780251c40
Xinzhe Li <xinli@naf02.ict.usc.edu>
2018-02-26 16:41:10 -0800

    actions

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {selectBook} from "../actions/index";
-import {bindActionCreators} from 'react';
+import {bindActionCreators} from 'redux';
 
 class BookList extends Component {
     renderList() {
@@ -38,4 +38,6 @@ function mapDispatchToProps(dispatch) {
     return bindActionCreators({selectBook: selectBook}, dispatch);
 }
 
+// Promote BookList from a component to a container - it needs to know about this new dispatch
+// method, selectBook. Make it available as a prop.
 export default connect(mapStateToProps, mapDispatchToProps)(BookList);
